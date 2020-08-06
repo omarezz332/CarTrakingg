668896447cfa9f2fb51c6c69e5613b48874da586
omarezz332 <omar_elsharkawy100@yahoo.com>
2020-08-06 16:20:39 +0200

    Signed-off-by: omarezz332 <omar_elsharkawy100@yahoo.com>

--- app/src/main/java/com/example/mapmycar/MapActivity.java
+++ app/src/main/java/com/example/mapmycar/MapActivity.java
@@ -1,10 +1,15 @@
 package com.example.mapmycar;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.location.LocationListener;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -18,6 +23,8 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.example.mapmycar.LoginAndRigster.Register;
+import com.example.mapmycar.LoginAndRigster.User;
 import com.example.mapmycar.directionhelpers.FetchURL;
 import com.example.mapmycar.directionhelpers.TaskLoadedCallback;
 import com.google.android.gms.common.ConnectionResult;
@@ -38,7 +45,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
@@ -58,15 +67,24 @@ import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRe
 import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse;
 import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
 import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback , TaskLoadedCallback {//}, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback, GoogleMap.OnMarkerClickListener {//}, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
@@ -75,15 +93,16 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private LocationCallback locationCallback;
     private LocationRequest locationRequest;
     private View mapView;
-    private GoogleApiClient mGoogleApiClient;
+    private List<User> users;
     private MarkerOptions place1, place2;
-   private DatabaseManger manger= new DatabaseManger();
+    private DatabaseManger manger = new DatabaseManger();
     private Polyline currentPolyline;
-   private final float DEFAULT_ZOOM = 17;
-   private LatLng myplace,destnation;
-   private boolean flag=true;
-   private MarkerOptions markerOptions=new MarkerOptions();
-    private MarkerOptions markerOptions2=new MarkerOptions();
+    private final float DEFAULT_ZOOM = 17;
+    private LatLng myplace, destnation;
+    private boolean flag = true;
+    Marker dist,mycarpin;
+    private List<LatLng> carsLatLngs = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -108,45 +127,140 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     return;
                 }
                 mLastKnownLocation = locationResult.getLastLocation();
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
-                manger.setCarlocation(mLastKnownLocation.getLongitude(),mLastKnownLocation.getLatitude());
-                Toast.makeText(getBaseContext(), "new location " + mLastKnownLocation.getLongitude() + " " + mLastKnownLocation.getLatitude(), Toast.LENGTH_LONG);
-                myplace=new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+               // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                manger.setCarlocation(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+                myplace = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+
             }
         };
 
-
-
-
-
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         mFusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());///////
-    }
 
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.setMyLocationEnabled(true);
-        mMap.getUiSettings().setMyLocationButtonEnabled(true);
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
-        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+        getData();
+    }
+    private void getData() {
+
+       int height = 130;
+        int width = 130;
+        BitmapDrawable bitmapdraw=(BitmapDrawable)getResources().getDrawable(R.drawable.car);
+        Bitmap b=bitmapdraw.getBitmap();
+        final Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        // DatabaseReference database = FirebaseDatabase.getInstance().getReference().child("Users");
+        FirebaseDatabase.getInstance().getReference().child("Users").addValueEventListener(new ValueEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
-            public void onMapLongClick(LatLng latLng) {
-                mMap.clear();
-                destnation=latLng;
-                markerOptions.position(destnation);
-                mMap.addMarker(markerOptions);
-                markerOptions2.position(myplace);
-                mMap.addMarker(markerOptions);
-                new FetchURL(MapActivity.this).execute(getUrl(markerOptions2.getPosition(), markerOptions.getPosition(), "driving"), "driving");
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                LatLng userlocation;
+                carsLatLngs.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    User user = dataSnapshot.getValue(User.class);
+
+                    assert user != null;
+                   // LatLng location=new LatLng(user.latitude, user.longitude);
+                     LatLng newLocation = new LatLng(dataSnapshot.child("latitude").getValue(double.class), dataSnapshot.child("longitude").getValue(double.class));
+                    userlocation = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+                    mMap.clear();
+                   //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 13));
+
+                    //Toast.makeText(MapActivity.this, String.valueOf(location), Toast.LENGTH_SHORT).show();
+                    if (user.getState().equals("Car")) {
+                        //userlocation = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+                        carsLatLngs.add(newLocation);
+
+                    }
+                    showMyCar(carsLatLngs);
+                }
+
+                //Toast.makeText(MapActivity.this, carsLatLngs.size(), Toast.LENGTH_SHORT).show();
+            }
+
+            private void showMyCar(List<LatLng> carsLatLngs) {
+                for (LatLng latLng : carsLatLngs) {
+                    Toast.makeText(MapActivity.this, String.valueOf(latLng), Toast.LENGTH_SHORT).show();
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("car")).
+                            setIcon(BitmapDescriptorFactory.fromBitmap(smallMarker));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18));
+
+                //mycarpin.setVisible(true);
+                //mycarpin.showInfoWindow();
+                 }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
-        place1 = new MarkerOptions().position(new LatLng(27.658143, 85.3199503)).title("Location 1");
-        place2 = new MarkerOptions().position(new LatLng(27.667491, 85.3208583)).title("Location 2");
+    }
 
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+       mMap = googleMap;
+       mMap.setOnMarkerClickListener( this);
+        mMap.setMapType(R.raw.google_style);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+       /* mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                destnation=latLng;
+                dist = mMap.addMarker(new MarkerOptions().position(latLng).visible(true).alpha(.8f).title("Destnation"));
+                dist.showInfoWindow();
+                new FetchURL(MapActivity.this).execute(getUrl(myplace, destnation, "driving"), "driving");
+
+            }
+        });*/
+
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                // Setting the title for the marker.
+                // This will be displayed on taping the marker
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+
+                // Clears the previously touched position
+                googleMap.clear();
+
+                // Animating to the touched position
+                googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                // Placing a marker on the touched position
+                googleMap.addMarker(markerOptions);
+                new FetchURL(MapActivity.this).execute(getUrl(myplace, markerOptions.getPosition(), "driving"), "driving");
+            }
+        });
 
 
 
@@ -161,6 +275,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
             }
         });
+
+
+
     }
 
 
@@ -184,7 +301,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     }
                 });
     }
-    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+   private String getUrl(LatLng origin, LatLng dest, String directionMode) {
         // Origin of route
         String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
         // Destination of route
@@ -205,6 +322,11 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         if (currentPolyline != null)
             currentPolyline.remove();
         currentPolyline = mMap.addPolyline((PolylineOptions) values[0]);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
     }
 
 
--- app/src/main/java/com/example/mapmycar/directionhelpers/PointsParser.java
+++ app/src/main/java/com/example/mapmycar/directionhelpers/PointsParser.java
@@ -78,7 +78,7 @@ public class PointsParser extends AsyncTask<String, Integer, List<List<HashMap<S
                 lineOptions.color(Color.MAGENTA);
             } else {
                 lineOptions.width(20);
-                lineOptions.color(Color.BLUE);
+                lineOptions.color(Color.GREEN);
             }
             Log.d("mylog", "onPostExecute lineoptions decoded");
         }
